57acda0a69b39a6f4b589d64bf44ad1df670912a
PopeyedLocket <lucius.dickerson@gmail.com>
2018-11-13 00:54:53 -0500

    replicated .sh test script

--- tests/local_test.py
+++ tests/local_test.py
@@ -5,11 +5,16 @@ import sys
 
 	TO DO:
 
+		implement and debug the rest of the test
+			use the same variable names eric used to make it easier for him to read
+				.. or as many as possible
 
+		ask Sydney what OS she uses and if everything was installed properly:
+			eos
+			eosfactory
 
 	'''
 
-# CONTRACT_PATH = '~/rooms/BOID/Token-Staking-Upgrade'
 CONTRACT_PATH = sys.path[0] + '/../'
 
 if __name__ == '__main__':
@@ -20,10 +25,10 @@ if __name__ == '__main__':
 	# create master account from which
 	# other account can be created
 	# accessed via global variable: master
-	create_master_account("master")
+	create_master_account('master')
 
 	# 3) Create 6 accounts: eosio_token, boid, boid_stake, boid_power, acct1, acct2
-	create_account('eosio_token', master, account_name='eosio.token')
+	# create_account('eosio_token', master, account_name='eosio.token')
 	create_account('boid', master, account_name='boid')
 	create_account('boid_stake', master, account_name='boid.stake')
 	create_account('boid_power', master, account_name='boid.power')
@@ -42,28 +47,60 @@ if __name__ == '__main__':
 
 	# deploy the token staking contract on the testnet
 	contract.deploy()
-	sys.exit()
 
 	############# now we can call functions ##############
 	########## (aka actions) from the contract! ##########
 
+	# Set up master as issuer of EOS and boid as issuer of BOID
 	# account.push_action(
 	#		action_name,
 	#		action_arguments_in_json,
-	#		account_whose_permission_is_needed
-	# )
+	#		account_whose_permission_is_needed)
 	host.push_action(
-		'create', {
-			'issuer': master,
-			'maximum_supply': '1000000000.0000 EOS'
-		}
-	)
-	# host.push_action(
-	# 	'create', {
-	# 		'issuer': master,
-	# 		'maximum_supply': "boid", "1000000000.0000 BOID"
-	# 	}
-	# )
+		'create',
+		{'issuer': master,
+			'maximum_supply': '1000000000.0000 EOS'},
+		[master, host])
+
+	host.push_action(
+		'create',
+		{'issuer': boid,
+			'maximum_supply': '1000000000.0000 BOID'},
+		[boid, host])
+
+	# Distribute initial quantities of EOS & BOID to test accounts
+	host.push_action(
+		'issue',
+		{'to': acct1,
+		'quantity': '1000.0000 EOS',
+		'memo': 'memo'},
+		[master])
+	host.push_action(
+		'issue',
+		{'to': acct2,
+		'quantity': '2000.0000 EOS',
+		'memo': 'memo'},
+		[master])
+	host.push_action(
+		'issue',
+		{'to': acct1,
+		'quantity': '1000.0000 BOID',
+		'memo': 'memo'},
+		[boid])
+	host.push_action(
+		'issue',
+		{'to': acct2,
+		'quantity': '2000.0000 BOID',
+		'memo': 'memo'},
+		[boid])
+
+	# Set up boid staking contract to boid.stake
+
+	# Set up boid power contract to boid.power
+
+	# Run staking tests with test1 and test2
 
 
+	# stop the testnet
+	stop()
 
